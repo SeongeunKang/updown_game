93d3e0ea9dd3c79bd6b38658b6839e5b75649824
Seongeun <sekang2018@gmail.com>
2023-02-22 12:37:36 +1100

    레이아웃 연결.

--- app/src/main/java/com/example/updowngame/MainActivity.kt
+++ app/src/main/java/com/example/updowngame/MainActivity.kt
@@ -1,1 +1,20 @@
-println {"Hello world"}
+package com.example.updowngame
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var buttonSubmit : Button
+    lateinit var editText: EditText
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_main2)
+
+        buttonSubmit = findViewById(R.id.btn_submit)
+        editText = findViewById(R.id.edt_number)
+    }
+}
